504c8140784968a7c66109eecc3d02c09c099dd8
abhishek-3009 <abhishekshar.3009@gmail.com>
2019-01-29 15:39:14 -0800

    Updated to cast int to char

--- src/org/apex/work/Problem9.java
+++ src/org/apex/work/Problem9.java
@@ -4,17 +4,21 @@ public class Problem9 {
 
 	public static void main(String[] args) {
 
-		StringBuilder strBuilder= new StringBuilder("This is me helooooooo and you are you");
-		System.out.println(strBuilder);
+		StringBuilder strBuilder= new StringBuilder();
+		int i=97;
 		System.out.println(strBuilder.capacity()+"\t"+strBuilder.length());
-		strBuilder.append("\nIs this Me? or is this you?");
+		while(i<=122) {
+			strBuilder.append((char)i);
+			i++;	
+		}
 		System.out.println(strBuilder);
 		System.out.println(strBuilder.capacity()+"\t"+strBuilder.length());
 		
-		StringBuffer strBuffer= new StringBuffer("This is Me helooooooo and you are you");
-		System.out.println(strBuffer);
+		StringBuffer strBuffer= new StringBuffer();
 		System.out.println(strBuffer.capacity()+"\t"+strBuffer.length());
-		strBuffer.append("\nIs this Me? or is this you?");
+		for(i=97;i<123;i++) {
+			strBuffer.append((char)i);
+		}
 		System.out.println(strBuffer);
 		System.out.println(strBuffer.capacity()+"\t"+strBuffer.length());
 	}
